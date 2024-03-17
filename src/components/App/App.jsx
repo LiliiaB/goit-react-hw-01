4dfdf0c790043edbf2ac2a0a42010e8380996889
Lilia Levchenko <liliya.benatti@gmail.com>
2024-03-17 18:02:17 +0100

    1 and 2

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Profile } from "../Profile";
+import userData from "../userData.json";
+import { FriendList } from "../FriendList/FriendList";
+import friends from "../friends.json";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </>
+  );
+}
